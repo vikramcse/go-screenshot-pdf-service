e1c48f85d77cf97ab16d1c13c3706818a501b11a
vikram Jadhav <vikramcse.10@gmail.com>
2020-05-10 17:05:10 +0530

    Update main_test.go

--- server/main_test.go
+++ server/main_test.go
@@ -3,6 +3,6 @@ package main
 import "testing"
 
 func TestDummy(t *testing.T) {
-	t.Log("This is a dummy test. I will be adding test cases here after figuring out, how testing works in chromedp!!")
+	t.Log("This is a dummy test. I will be adding test cases here after figuring out, how testing works in chromedp!!!")
 
 }
